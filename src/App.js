17bfc4801fe8cd40e7e5534f3b81788eabb9778f
habibmustafa <hebibullahmustafazade@gmail.com>
2022-05-26 12:38:23 +0400

    fix: reset

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import IntroPage from './pages/IntroPage/IntroPage'
-import MainPage from './pages/MainPage/MainPage'
 
 export default function App() {
    return (
-      <BrowserRouter>
-         <Routes>
-            <Route path='/' element={<IntroPage />} />
-            <Route path='/taskdo' element={<MainPage />} />
-         </Routes>
-      </BrowserRouter>
+      <div>
+         App
+      </div>
    )
 }
