fc55a3394c67761ace7b0a18f7683e4ae1f238a2
habibmustafa <hebibullahmustafazade@gmail.com>
2022-06-13 17:17:11 +0400

    add: tailwind includeLanguages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function App() {
    return (
-      <div>App</div>
+      <div className='bg-gray-400 text-neutral-800 font-semibold text-2xl'>
+         App
+      </div>
    )
 }
