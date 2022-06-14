2ebfc001d5fc499bb21c4de49a9e62076be53339
habibmustafa <hebibullahmustafazade@gmail.com>
2022-06-14 11:25:34 +0400

    fix: app bg-color

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function App() {
    return (
       <div className='h-screen bg-slate-200'>
-         <div className='text-xl font-medium h-15 bg-emerald-300'>App</div>
+         <div className='text-xl font-medium h-15 bg-emerald-400'>App</div>
       </div>
    )
 }
