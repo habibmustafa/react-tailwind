fc19915dc7a8a805a367c68e96f6aea149da5b30
habibmustafa <hebibullahmustafazade@gmail.com>
2022-06-14 11:45:17 +0400

    add: manual inline px

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function App() {
    return (
       <div className='h-screen bg-slate-200'>
-         <div className='text-xl font-medium h-15 bg-emerald-400'>App</div>
+         <div className='text-[20px] font-medium h-15 bg-emerald-400'>App</div>
       </div>
    )
 }
