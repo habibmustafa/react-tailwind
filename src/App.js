6067d3620359809e66689620c411bdc0ccf15890
habibmustafa <hebibullahmustafazade@gmail.com>
2022-06-16 15:48:56 +0400

    add: button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ export default function App() {
    return (
       <div className='h-screen bg-slate-200'>
          <div className='text-[20px] font-medium h-15 bg-emerald-400'>App</div>
+         <button className='px-3 py-1 rounded mt-2 ml-2 text-orange-100 shadow text-xl bg-orange-500'>Click</button>
       </div>
    )
 }
